03b132d05af22870756040050ce3fa6a55c510f2
Sojib Islam <sojibislam6505@gmail.com>
2025-11-21 18:33:01 +0600

    explore navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import './App.css'
 import Header from './Components/Header/Header'
 
 
 function App() {
- 
+  const navigation = useNavigation();
+  const isNavigation =  Boolean(navigation.location)
 
   return (
     <>
      <Header></Header>
+     {isNavigation && <span className='text-7xl font-bold flex justify-center items-center'>loading....</span>}
       <Outlet></Outlet>
     </>
   )
